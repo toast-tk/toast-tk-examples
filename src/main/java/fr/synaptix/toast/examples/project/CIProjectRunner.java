23e9f8c7ab71eab6d4bb5fdf3f56edbdb61f67bf
Nicolas Sauvage <nicolas.sauvage@synaptix-labs.com>
2015-11-19 17:30:33 +0100

    update test to run without mongo, from a campaign file

--- src/main/java/fr/synaptix/toast/examples/project/CIProjectRunner.java
+++ src/main/java/fr/synaptix/toast/examples/project/CIProjectRunner.java
@@ -1,5 +1,8 @@
 package fr.synaptix.toast.examples.project;
 
+import com.synaptix.toast.dao.domain.impl.test.block.IProject;
+import com.synaptix.toast.runtime.parse.ScenarioParser;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.inject.Guice;
@@ -7,46 +10,53 @@ import com.synaptix.toast.runtime.AbstractProjectRunner;
 
 import fr.synaptix.toast.examples.boot.BootModule;
 
+import java.net.URL;
+
 public class CIProjectRunner extends AbstractProjectRunner {
 
-	public CIProjectRunner() throws Exception {
-		super(Guice.createInjector(new BootModule()));
-	}
-	
-	public static void main(String[] args) {
-		CIProjectRunner projectRunner;
-		try {
-			projectRunner = new CIProjectRunner();
-			projectRunner.test("project", true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void runTest(){
-		CIProjectRunner.main(null);
-	}
-	
-	@Override
-	public void tearDownEnvironment() {
-		// TODO Auto-generated method stub
-	}
+    public CIProjectRunner() throws Exception {
+        super(Guice.createInjector(new BootModule()));
+    }
 
-	@Override
-	public void initEnvironment() {
-		// TODO Auto-generated method stub
-	}
+    public static void main(String[] args) {
+        CIProjectRunner projectRunner;
+        try {
+            projectRunner = new CIProjectRunner();
+            URL testFileUrl = CIProjectRunner.class.getClassLoader().getResource("campaigns/project1.test");
+            Assert.assertNotNull(testFileUrl);
+            String path = testFileUrl.getPath();
+            ScenarioParser scenarioParser = new ScenarioParser();
+            IProject project = scenarioParser.parse(path);
+            projectRunner.execute(project, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void beginTest() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Test
+    public void runTest() {
+        CIProjectRunner.main(null);
+    }
 
-	@Override
-	public void endTest() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void tearDownEnvironment() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void initEnvironment() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void beginTest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void endTest() {
+        // TODO Auto-generated method stub
+
+    }
 }
